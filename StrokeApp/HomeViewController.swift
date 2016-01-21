c34c6b22e9bc067dcf5e0ec53af78ea75009a52e
Chase Woodford <chase@chasewoodford.com>
2016-01-21 14:55:39 -0500

    add home view and controller

--- StrokeApp/HomeViewController.swift
+++ StrokeApp/HomeViewController.swift
@@ -1,25 +1,29 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  StrokeApp
 //
 //  Created by Chase Woodford on 1/21/16.
 //  Copyright © 2016 Chase Woodford. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var CurrentRiskFactorLabel: UILabel!
+    @IBOutlet weak var CurrentRiskFactorImage: UIImageView!
+    @IBOutlet weak var StrokeInfoButton: UIButton!
+    @IBOutlet weak var StrokeTestsButton: UIButton!
+    @IBOutlet weak var SendToDoctorButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
